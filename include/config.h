1e66443eea42211fb26100dc5d73e789ce0c38b6
Reinhard Lenz <reinhard.lenz@konecranes.com>
2026-07-05 17:24:32 +0300

    first changes  commit and fatal error void added

--- include/config.h
+++ include/config.h
@@ -16,11 +16,14 @@ constexpr uint32_t BNO_BAUD = 3000000;
 
 constexpr gpio_num_t PIN_BNO_TX    = GPIO_NUM_14;
 constexpr gpio_num_t PIN_BNO_RX    = GPIO_NUM_15;
-constexpr gpio_num_t PIN_BNO_RESET = GPIO_NUM_13;
 
 // -------------------------------
 // LED pin for status indication (blinking)
 // -------------------------------
-constexpr gpio_num_t PIN_STATUS_LED = GPIO_NUM_4;
+constexpr gpio_num_t PIN_STATUS_LED = GPIO_NUM_13;
+
+
+constexpr uint32_t RESET_TIME_MS = 10;
+constexpr uint32_t BLINK_DELAY  = 200;
 
 #endif
--- src/main.cpp
+++ src/main.cpp
@@ -1,28 +1,73 @@
 #include <Arduino.h>
 #include <Adafruit_BNO08x.h>
-
+#include <config.h>
 #define BNO08X_RESET -1
-#define RX2 15
-#define TX2 14
+
+constexpr char PROGRAM_NAME[] = "Stage 1A UART Diagnostic";
+constexpr char VERSION[]      = "0.1.0";
+constexpr char BUILD_DATE[]   = __DATE__;
+constexpr char BUILD_TIME[]   = __TIME__;
 
 Adafruit_BNO08x  bno08x(BNO08X_RESET);
 sh2_SensorValue_t sensorValue;
 
+enum class ErrorCode
+{
+    None,
+    UART,
+    BNO_NotFound,
+    EnableReport,
+    ProductID,
+};
+
+void fatalError(ErrorCode code)
+{
+    Serial.println();
+    Serial.println("========== FATAL ERROR ==========");
+
+    switch (code)
+    {
+        case ErrorCode::UART:
+            Serial.println("Unable to communicate with BNO085.");
+            break;
+
+        case ErrorCode::BNO_NotFound:
+            Serial.println("BNO085 not detected.");
+            break;
+
+        case ErrorCode::EnableReport:
+            Serial.println("Could not enable report.");
+            break;
+
+        default:
+            Serial.println("Unknown error.");
+            break;
+    }
+
+    while (true)
+    {
+        digitalWrite(PIN_STATUS_LED, HIGH);
+        delay(BLINK_DELAY);
+        digitalWrite(PIN_STATUS_LED, LOW);
+        delay(BLINK_DELAY);
+    }
+}
+
 void setReports(void);
 
 void setup(void) {
-  Serial.begin(115200);
-  Serial2.begin(3000000, SERIAL_8N1, RX2, TX2);
-  while (!Serial) delay(10);     // will pause Zero, Leonardo, etc until serial console opens
-  while (!Serial2) delay(10); 
+  Serial.begin(PC_BAUD);
+  Serial2.begin(BNO_BAUD, SERIAL_8N1, PIN_BNO_RX, PIN_BNO_TX);
+  while (!Serial) delay(RESET_TIME_MS);     
+  while (!Serial2) delay(RESET_TIME_MS); 
 
   Serial.println("Adafruit BNO08x Accelerometer test!");
 
   // Try to initialize!
-  if (!bno08x.begin_UART(&Serial2)) {  // Requires a device with > 300 byte UART buffer!
-    Serial.println("Failed to find BNO08x chip");
-    while (1) { delay(10); }
+  if (!bno08x.begin_UART(&Serial2)) {  
+    fatalError(ErrorCode::BNO_NotFound);
   }
+
   Serial.println("BNO08x Found!");
 
   setReports();
